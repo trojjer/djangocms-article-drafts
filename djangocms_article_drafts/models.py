c5cac63c60ebc17229b7280c5410447a3680a592
Noel da Costa <noel@arc2.co.uk>
2018-05-04 14:00:21 +0100

    Implemented ContentTypes for Publishable model.
    - and first test of Publishable Model

--- djangocms_article_drafts/models.py
+++ djangocms_article_drafts/models.py
@@ -1,13 +1,30 @@
-from cms.exceptions import PublicIsUnmodifiable
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+from cms.exceptions import PublicIsUnmodifiable
+from cms.signals import post_publish
 
 
-class BasePublishable(models.Model):
+class Publishable(models.Model):
     """Abstract model to represent a publishable CMS node e.g. Page, Article.
     """
     
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    is_draft = models.BooleanField(default=True)
+
+    class Meta: 
+        unique_together = ('content_type', 'object_id')
+
+# @todo: db keeping track of registered classes / models
+# class StoredPublishables(models.Model):
+    
+    
+class PublishPool(object):
     def __init__(self):
         self.model_pool = {}
     
@@ -15,30 +32,29 @@ class BasePublishable(models.Model):
         # todo: check that the model has certain required attributes e.g. publisher_is_draft
         self.model_pool[model_class.__name__] = model_class
 
-    def publish(self, language):
+    def publish(self, model_instance):
         # Publish can only be called on draft pages
-        if not self.publisher_is_draft:
+        if not model_instance.is_draft:
             raise PublicIsUnmodifiable('The public instance cannot be published. Use draft.')
 
         return True
-        
+    
+    
+@receiver(post_publish, dispatch_uid='publishable_post_publish')
+def publish_receiver(sender, **kwargs):
+    # check for known publishable models
+    if sender in publishable_pool.model_pool.values():
+        publishable_pool.publish(sender)
+    # e.g. model.save()
+    
+@receiver(post_save, dispatch_uid="publishable_post_save")
+def save_receiver(sender, **kwargs):
+    if sender in publishable_pool.model_pool.values():
+        if kwargs['created']: 
+            # import pdb; pdb.set_trace()
+            Publishable.objects.create(content_object=kwargs['instance'])
+        else:
+            # @todo: check for existence of records
+            pass
 
-publishable_pool = BasePublishable()    
-
-
-# @todo
-# @receiver(pre_publish, dispatch_uid='generic_publish_event')
-# def publish_event(sender, instance, **kwargs):
-#     # check the settings PUBLISHER_REGISTERED_MODELS for known publishable models
-
-#     # get the model
-#     # e.g. model.save()
-#     pass
-
-# @receiver(post_publish, dispatch_uid='generic_publish_event')
-# def publish_event(sender, instance, **kwargs):
-#     # check the settings PUBLISHER_REGISTERED_MODELS for known publishable models
-
-#     # get the model
-#     # e.g. model.save()
-#     pass
+publishable_pool = PublishPool()    
--- tests/tests.py
+++ tests/tests.py
@@ -1,13 +1,15 @@
 from unittest.mock import MagicMock, patch
 
 from django.db import models
+from django.db.models.signals import post_save
 from django.test import TestCase
+from django.contrib.contenttypes.models import ContentType
+
 from cms.exceptions import PublicIsUnmodifiable
+from cms.signals import post_publish
 
-
-from djangocms_article_drafts.models import BasePublishable, publishable_pool
+from djangocms_article_drafts.models import Publishable, publishable_pool
 from djangocms_article_drafts.test_project.models import ArticleTest
-
 
 class GenericPublishingTestCase(TestCase):
 
@@ -19,8 +21,23 @@ class GenericPublishingTestCase(TestCase):
         publishable_pool.register(ArticleTest)
         self.assertEquals(publishable_pool.model_pool, {ArticleTest.__name__: ArticleTest})
 
+    # def test_publish_signal(self):
+    #     post_publish.send(ArticleTest.objects.create())
+
+    # def test_publish_signal_unregistered_model(self):
+    #     post_publish.send(object())
     
-    
+    def test_save_signal_creates_new_publishable(self):
+        # article = ArticleTest.objects.create()
+        # import pdb; pdb.set_trace()
+        # publishable_type = ContentType.objects.get(app_label="djangocms_article_drafts", model="publishable")
+        article = ArticleTest()
+        article.save()
+        # post_save.send(ArticleTest, instance=article, created=True)
+        self.assertEquals(Publishable.objects.count(), 1)
+        
+        publishable = Publishable.objects.first()
+        self.assertEquals(publishable.object_id, article.id)
 
     #todo: refactor for pool
     # def test_exception_if_article_is_draft(self):
